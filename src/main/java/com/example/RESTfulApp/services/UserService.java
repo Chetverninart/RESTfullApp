9eed06f06ac94a01682124e35bef7e9dba9cac34
chetverninart <chetverninart@gmail.com>
2021-09-09 13:52:38 +0300

    Commit

--- src/main/java/com/example/RESTfulApp/services/UserService.java
+++ src/main/java/com/example/RESTfulApp/services/UserService.java
@@ -7,9 +7,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
-import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
